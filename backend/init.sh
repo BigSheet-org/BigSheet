28afad1e2d7a57e2c151dfddc0abd42cb03dfb12
Maxime Marcelin <maisonmarc7@gmail.com>
2023-10-23 00:33:52 +0200

    Trying again ....

--- backend/init.sh
+++ backend/init.sh
@@ -1,6 +1,41 @@
-#!./bin/bash
+#!/bin/bash
+
+# Params : 
+# $1 : key
+# $2 : string to replace
+# $3 : file_path
+function write_key(){
+    # Reading file content.
+    file_content=$(<"$3")
+    # Placing keys.
+    modified_content="${$3//$2/$1}"
+    echo "$modified_content" > "$3"
+}
+
+
 echo "[CONFIG] - Starting project's configuration... "
 
-# Copying files.
-cp ./examples/init_db.sql.example ./init_db.sql
-cp ./examples/.env.example ./.env
+$POSTGRES_KEY=$(openssl rand -hex 32)
+$REDIS_KEY=$(openssl rand -hex 32)
+$JWT_AUTH_KEY=$(openssl rand -hex 32)
+$JWT_REFRESH_KEY=$(openssl rand -hex 32)
+
+echo "[CONFIG] - Copying files... "
+
+cp -f ./examples/init_db.sql.example ./init_db.sql 
+cp -f ./examples/.env.example ./.env
+
+echo "[CONFIG] - Installing keys ..."
+
+# PostgreSQL key
+write_key($POSTGRES_KEY "postgres_key_to_replace" '.\.env')
+write_key($POSTGRES_KEY "postgres_key_to_replace" '.\init_db.sql')
+
+# Redis key
+write_key($REDIS_KEY "redis_key_to_replace" '.\.env')
+
+# JWT Keys
+write_key($JWT_AUTH_KEY "jwt_auth_key_to_replace" '.\.env')
+write_key($JWT_REFRESH_KEY "jwt_refresh_key_to_replace" '.\.env')
+
+echo "[CONFIG] - Configuration done !"

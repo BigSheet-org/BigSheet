2c7ce7a26c6e93dc3121b8a98ca596eec96df5ee
Maxime Marcelin <maisonmarc7@gmail.com>
2023-10-23 01:47:55 +0200

    Finally ! It's alive !
    Added configuration for unix systems.

--- backend/init.sh
+++ backend/init.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
 
-# Params : 
+# Params :
 # $1 : key
 # $2 : string to replace
 # $3 : file_path
-function write_key(){
-    # Reading file content.
-    file_content=$(<"$3")
-    # Placing keys.
-    modified_content="${$3//$2/$1}"
-    echo "$modified_content" > "$3"
+write_key() {
+    sed -i "s/$2/$1/g" $3
 }
-
 
 echo "[CONFIG] - Starting project's configuration... "
 
-$POSTGRES_KEY=$(openssl rand -hex 32)
-$REDIS_KEY=$(openssl rand -hex 32)
-$JWT_AUTH_KEY=$(openssl rand -hex 32)
-$JWT_REFRESH_KEY=$(openssl rand -hex 32)
+POSTGRES_KEY=$(openssl rand -hex 32)
+REDIS_KEY=$(openssl rand -hex 32)
+JWT_AUTH_KEY=$(openssl rand -hex 32)
+JWT_REFRESH_KEY=$(openssl rand -hex 32)
 
 echo "[CONFIG] - Copying files... "
 
-cp -f ./examples/init_db.sql.example ./init_db.sql 
+cp -f ./examples/init_db.sql.example ./init_db.sql
 cp -f ./examples/.env.example ./.env
 
 echo "[CONFIG] - Installing keys ..."
 
 # PostgreSQL key
-write_key($POSTGRES_KEY "postgres_key_to_replace" '.\.env')
-write_key($POSTGRES_KEY "postgres_key_to_replace" '.\init_db.sql')
+write_key $POSTGRES_KEY "postgres_key_to_replace" './.env'
+write_key $POSTGRES_KEY "postgres_key_to_replace" './init_db.sql'
 
 # Redis key
-write_key($REDIS_KEY "redis_key_to_replace" '.\.env')
+write_key $REDIS_KEY "redis_key_to_replace" './.env'
 
 # JWT Keys
-write_key($JWT_AUTH_KEY "jwt_auth_key_to_replace" '.\.env')
-write_key($JWT_REFRESH_KEY "jwt_refresh_key_to_replace" '.\.env')
+write_key $JWT_AUTH_KEY "jwt_auth_key_to_replace" './.env'
+write_key $JWT_REFRESH_KEY "jwt_refresh_key_to_replace" './.env'
 
 echo "[CONFIG] - Configuration done !"

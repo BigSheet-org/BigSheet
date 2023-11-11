e34b0b90d93a7d826e4948147868eaff1175e27a
Maxime Marcelin <maisonmarc7@gmail.com>
2023-11-11 22:36:52 +0100

    Added Strict mode in Javascript files. Added A check to see if the user is connected in the home page.

--- frontend/src/components/HomePageContent.vue
+++ frontend/src/components/HomePageContent.vue
@@ -1,18 +1,25 @@
 <script>
 import Routes from "../assets/static/Routes.js";
+import User from "../scripts/DAO/User.js";
 
 export default {
     computed: {
         Routes() {
             return Routes
+        },
+        CheckAuthentication() {
+            return User.isUserConnected();
         }
+    },
+    methods: {
+
     }
 
 }
 </script>
 
 <template>
-    <div>
+    <div v-if="!this.CheckAuthentication">
         <h1>Veuillez vous authentifier pour continuer.</h1>
         <router-link :to="Routes.CONNEXION.path">
             <button>Connexion</button>
@@ -20,5 +27,9 @@ export default {
         <router-link :to="Routes.INSCRIPTION.path">
             <button>Inscription</button>
         </router-link>
+    </div>
+
+    <div v-else>
+        <h1>Bienvenue !</h1>
     </div>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index.js";

12f034787a33eed8527a7610502b061cad03be47
Maxime Marcelin <maisonmarc7@gmail.com>
2023-11-17 16:21:46 +0100

    Forgot to not show immediatly the popup.

--- frontend/src/components/forms/InscriptionForm.vue
+++ frontend/src/components/forms/InscriptionForm.vue
@@ -130,7 +130,7 @@ export default {
                 confirmPassword: false,
             },
             loading: false,
-            showPopup: true,
+            showPopup: false,
         }
     }
 }
--- frontend/src/scripts/DAO/User.js
+++ frontend/src/scripts/DAO/User.js
@@ -1,5 +1,4 @@
 import {api} from './API.js'
-import Data from "../../assets/static/Data.js";
 
 class User {
 

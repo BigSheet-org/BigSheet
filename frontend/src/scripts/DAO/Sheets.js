c92a62423197c2bb73b2d083c42b15cfb64cd5de
Clement.JL <118436575+aildontknow@users.noreply.github.com>
2023-11-27 01:30:33 +0100

    Modify + add export to Sheets.js

--- frontend/src/scripts/DAO/Sheets.js
+++ frontend/src/scripts/DAO/Sheets.js
@@ -7,25 +7,32 @@ class Sheets {
                     {
                         name: "Sheet1",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
-                        details: "Test Sheet N°1"
+                        details: "Test Sheet N°1",
+                        owner: "Test"
                     },
                     {
                         name: "Sheet2",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
-                        details: "Test Sheet N°2"
+                        details: "Test Sheet N°2",
+                        owner: "Test3"
                     },
                     {
                         name: "Sheet3",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
-                        details: "Test Sheet N°3"
+                        details: "Test Sheet N°3",
+                        owner: "Test"
                     },
                     {
                         name: "Sheet4",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
-                        details: "Test Sheet N°4"
+                        details: "Test Sheet N°4",
+                        owner: "Test2"
                     }
-                ])},
+                ])
+            },
                 1000)        // We return the object after 1 second.
         })
     }
 }
+
+export default Sheets

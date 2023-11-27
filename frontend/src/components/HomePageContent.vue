c5bc2a85b2be10fd92b5d80868f7f40602783881
Clement.JL <118436575+aildontknow@users.noreply.github.com>
2023-11-27 01:31:38 +0100

    add user's sheet list + create sheet button

--- frontend/src/components/HomePageContent.vue
+++ frontend/src/components/HomePageContent.vue
@@ -1,11 +1,17 @@
 <script>
 import User from "../scripts/DAO/User.js";
+import Sheets from "../scripts/DAO/Sheets.js"
 import Routes from "../assets/static/Routes.js";
+import SheetList from "./SheetList.vue";
 
 export default {
+    components: {
+        SheetList
+    },
     data() {
         return {
             user: null,
+            sheets: [],
         }
     },
     computed: {
@@ -15,11 +21,15 @@ export default {
     methods: {
         async fetchUserData() {
             this.user = await User.fetchUserData();
-        }
+        },
+        async fetchSheets() {
+            this.sheets = await Sheets.getOwnedSheets();
+        },
     },
     beforeMount() {
         if (this.CheckAuthentication) {
             this.fetchUserData();
+            this.fetchSheets();
         }
     }
 
@@ -38,6 +48,18 @@ export default {
     </div>
 
     <div v-else>
-        <h1>Bienvenue {{user !== null ? user.firstname : "" }} !</h1>
+        <h1>Bienvenue</h1>
+        <div v-if="sheets.length > 0">
+            <sheet-list :sheets="sheets"></sheet-list>
+        </div>
+        <div v-else>
+            <p>Vous n'avez pas encore créé de feuilles de calcul.</p>
+        </div>
+        <!--<router-link>-->
+        <button style="position: fixed; right: 1.1rem; bottom: 1.3rem; border-radius: 50%;">
+            +
+            <!--<img src="../assets/pictures/icons/Ajouter.png" alt="Créer un nouveau tableau">-->
+        </button>
+        <!--</router-link>-->
     </div>
 </template>

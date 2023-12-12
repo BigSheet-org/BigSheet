3528446203e7fe1c1d412e72decd2fbffd13a2b2
Maxime Marcelin <maisonmarc7@gmail.com>
2023-12-12 16:43:18 +0100

    Applied requested changes by @Fulash Co-authored-by: @aildontknow

--- frontend/src/scripts/DAO/Sheets.js
+++ frontend/src/scripts/DAO/Sheets.js
@@ -5,55 +5,55 @@ class Sheets {
             setTimeout(() => {
                 resolve([
                     {
-                        name: "Sheet 1",
+                        title: "Sheet 1",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°1",
                         owner: "Test"
                     },
                     {
-                        name: "Sheet 2",
+                        title: "Sheet 2",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°2",
                         owner: "Test3"
                     },
                     {
-                        name: "Sheet 3",
+                        title: "Sheet 3",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°3",
                         owner: "Test"
                     },
                     {
-                        name: "Sheet 4",
+                        title: "Sheet 4",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°4",
                         owner: "Test2"
                     },
                     {
-                        name: "Sheet 5",
+                        title: "Sheet 5",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°5",
                         owner: "Test2"
                     },
                     {
-                        name: "Sheet 6",
+                        title: "Sheet 6",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°6",
                         owner: "Test2"
                     },
                     {
-                        name: "Sheet 7",
+                        title: "Sheet 7",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°7",
                         owner: "Test2"
                     },
                     {
-                        name: "Sheet 8",
+                        title: "Sheet 8",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°8",
                         owner: "Test2"
                     },
                     {
-                        name: "Sheet 9",
+                        title: "Sheet 9",
                         createdAt: "2023-11-20 00:39:50.434000 +00:00",
                         details: "Test Sheet N°9",
                         owner: "Test2"

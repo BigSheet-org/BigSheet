24a7e4c3da1c8d10e145df7e157c643adce54d2d
Maxime Marcelin <maisonmarc7@gmail.com>
2023-12-15 14:15:06 +0100

    Updated localStorage to use SessionStorage to store data.

--- frontend/src/scripts/DAO/LocalStorage.js
+++ frontend/src/scripts/DAO/LocalStorage.js
@@ -1,20 +1,20 @@
 class LocalStorage {
 
     static saveToLocalStorage(access_token, refresh_token){
-        window.localStorage.setItem("access_token", access_token);
-        window.localStorage.setItem("refresh_token", refresh_token);
+        window.sessionStorage.setItem("access_token", access_token);
+        window.sessionStorage.setItem("refresh_token", refresh_token);
     }
 
     static cleanLocalStorage(){
-        window.localStorage.clear();
+        window.sessionStorage.clear();
     }
 
     static getAccessToken() {
-        return window.localStorage.getItem("access_token");
+        return window.sessionStorage.getItem("access_token");
     }
 
     static getRefreshToken() {
-        return window.localStorage.getItem("refresh_token");
+        return window.sessionStorage.getItem("refresh_token");
     }
 }
 
